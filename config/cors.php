57d4d2f3c22c0f5e4be8ec1928f39f5a2bc8cce7
mohamedhussieen <mohamedhusssieen@gmail.com>
2025-08-19 17:08:18 +0300

    cors

--- config/cors.php
+++ config/cors.php
@@ -15,30 +15,20 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'broadcasting/auth'],
+    'paths' => ['api/*', 'broadcasting/auth', '*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'http://localhost:18000',
-        'http://127.0.0.1:18000',
-        'https://dashboard.ajza.net',
-        'https://ajza.net',
-        'https://dev.ajza.net',
-        'https://www.dev.ajza.net',
-        // Add your frontend domains here
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'],
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
